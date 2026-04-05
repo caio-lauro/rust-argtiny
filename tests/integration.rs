796b8314c0aa6e279fc5a5344ad4edcdda07294a
caio-lauro <caiolaurodelima@gmail.com>
2026-04-05 12:24:00 -0300

    fix formatting on integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,18 +1,44 @@
-use argparser::{Argument, ArgumentParser, ArgumentType::*, OptionalArgument, ParsedValue, ParseError};
+use argparser::{
+    Argument, ArgumentParser, ArgumentType::*, OptionalArgument, ParseError, ParsedValue,
+};
 
 #[test]
 fn full_usage() -> Result<(), Box<dyn std::error::Error>> {
     let parser = ArgumentParser::new()
         .add_arg(Argument::from("input", Text))
         .add_arg(Argument::from("output", Text))
-        .add_arg(OptionalArgument::from("verbose", None, Boolean, ParsedValue::Boolean(false)))
+        .add_arg(OptionalArgument::from(
+            "verbose",
+            None,
+            Boolean,
+            ParsedValue::Boolean(false),
+        ))
         .add_arg(Argument::from("port", Integer))
-        .add_arg(OptionalArgument::from("count", Some("c"), Integer, ParsedValue::Integer(1)));
-    let args = &["program", "input.txt", "--verbose", "output.txt", "8080", "-c", "42"];
+        .add_arg(OptionalArgument::from(
+            "count",
+            Some("c"),
+            Integer,
+            ParsedValue::Integer(1),
+        ));
+    let args = &[
+        "program",
+        "input.txt",
+        "--verbose",
+        "output.txt",
+        "8080",
+        "-c",
+        "42",
+    ];
     let parsed = parser.parse(args.map(|s| s.to_string()))?;
 
-    assert_eq!(parsed.get("input"), &ParsedValue::Text("input.txt".to_string()));
-    assert_eq!(parsed.get_value::<String>("output"), String::from("output.txt"));
+    assert_eq!(
+        parsed.get("input"),
+        &ParsedValue::Text("input.txt".to_string())
+    );
+    assert_eq!(
+        parsed.get_value::<String>("output"),
+        String::from("output.txt")
+    );
     assert_eq!(parsed.get_value::<i64>("port"), 8080);
     assert_eq!(parsed.get_value::<bool>("verbose"), true);
     assert_eq!(parsed.get_value::<i64>("count"), 42);
